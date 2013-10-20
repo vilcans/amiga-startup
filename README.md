ecbd73d83ebd52aa1c6b7c09525c5c98b5cb227e
Martin Vilcans <martin@librador.com>
2013-10-20 23:13:32 +0200

    Add assert macros

--- README.md
+++ README.md
@@ -8,3 +8,37 @@ Supports defensive programming using asserts in debug builds.
 
 System shutdown and demo startup were taken from Blueberry's
 [BlueBerryDemoStartup](http://ada.untergrund.net/forum/index.php?action=vthread&forum=4&topic=444)
+
+## Assert macros
+
+If DEBUG is defined, the assert* macros check whether a
+condition is true, and fails if it is not.
+Use this to make sure that an assumption you are making in
+your code is really true at runtime.
+
+For example, if you code assumes that the value of d1.w is 1,
+you can add a check like this:
+
+    assertw eq,#1,d1,$f00
+
+This checks (using cmp.w) whether d1 is 1.
+If d1 is not 1, freezes showing a screen with a red pattern.
+
+A common use for this is to check for buffer overruns:
+
+        lea someData,a0
+    someLoop:
+        move #someValue,(a0)+  ; etc.
+        dbra d0,someLoop  ;or similar
+        ; ls is the "lower or same" cc
+        assertl ls,#endOfData,a0,$ff0
+
+    someData:
+        dcb.l 100
+    endOfData:
+
+See debug.s for documentation of all macros.
+
+The assert macros do nothing if DEBUG is not defined,
+so you can be liberal with them,
+and not have to worry about the performance in a "release" build.
--- debug.s
+++ debug.s
@@ -11,3 +11,70 @@ failure        macro
 	btst	#10,$dff016
 	bne.s	.\@
 	endm
+
+; Fails if a condition fails.
+;
+; Usage:
+;     assert <cc>,<color>
+;
+; This checks whether the condition given in <cc> is valid,
+; and if it is not, fails.
+; For example:
+;
+;     assert eq,$f00
+;
+; This calls the failure macro with red color if the zero flag is not set.
+assert	macro
+	ifd	DEBUG
+	b\1.s	.\@
+	failure	\2
+.\@:
+	endc
+	endm
+
+; Asserts the comparison between 32 bit values.
+; Out of debug mode, does nothing.
+;
+; Example:
+;
+;     assertl eq,#endOfCopperlist,a1,$f00
+;
+; Checks that a1.l is equal to #endOfCopperlist
+; and calls failure with the color $f00 otherwise.
+assertl	macro
+	ifd	DEBUG
+	cmp.l	\2,\3
+	assert	\1,\4
+	endc
+	endm
+
+; Asserts the comparison between 16 bit values.
+; Out of debug mode, does nothing.
+;
+; Example:
+;
+;     assertw lt,#5,d0,$f00
+;
+; Checks that d0.w is less than 5 and
+; calls failure with the color $f00 otherwise.
+assertw	macro
+	ifd	DEBUG
+	cmp.w	\2,\3
+	assert	\1,\4
+	endc
+	endm
+
+; Asserts the comparison between 8 bit values.
+; Out of debug mode, does nothing.
+;
+; Example:
+;
+;     assertb eq,#127,d0,$f00
+;
+; Checks that d0.b is equal to 127 and
+; calls failure with the color $f00 otherwise.
+assertb	macro
+	ifd	DEBUG
+	cmp.b	\2,\3
+	assert	\1,\4
+	endm
