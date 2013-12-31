dc24e62ddc22ee71ad892f3ffc38c6412dfb3704
Martin Vilcans <martin@librador.com>
2013-12-31 07:17:01 +0300

    Move bitmap handling into separate Python module

--- bitplanify.py
+++ bitplanify.py
@@ -2,28 +2,19 @@
 
 import sys
 import math
-from array import array
-from PIL import Image
-
 import argparse
+from PIL import Image
+from bitmap import (
+    image_to_bitplanes,
+    write_bitplane,
+    write_interleaved,
+    to_amiga_colors,
+)
 
 
 def write_copper(file):
     for index, rgb in enumerate(palette):
         file.write('\tdc.w\t$%x,$%03x\n' % (0x180 + index * 2, rgb))
-
-
-def write_interleaved(file):
-    """Write all bitplanes in interleaved mode to a file."""
-    for row in xrange(height):
-        offset = row * byte_width
-        for plane_index in range(bit_depth):
-            bitplanes[plane_index][offset:offset + byte_width].write(file)
-
-
-def write_bitplane(file, plane_index):
-    """Write a single bitplane to a file."""
-    bitplanes[plane_index].write(out)
 
 
 parser = argparse.ArgumentParser(description='Convert an image to Amiga bitplanes')
@@ -67,42 +58,27 @@ if not bit_depth:
     if verbose:
         print 'Detected bit depth:', bit_depth
 
-palette = []
-for color_index in range(min(number_of_colors, 1 << bit_depth)):
-    r, g, b = [ord(c) for c in img.palette.palette[color_index * 3:(color_index + 1) * 3]]
-    rgb = ((r >> 4) << 8) | ((g >> 4) << 4) | (b >> 4)
-    palette.append(rgb)
+palette = to_amiga_colors(
+    img.palette.palette[:min(number_of_colors, 1 << bit_depth) * 3]
+)
 
 width, height = img.size
 if (width & 7) != 0:
     print 'Width is not divisable by 8:', width
     sys.exit(1)
 
-byte_width = (width + 7) // 8
-
+bitmap_image = image_to_bitplanes(img, bit_depth)
 if verbose:
     print 'Image width: %d pixels = %d bytes. Height: %d pixels' % (
-        width, byte_width, height)
-
-bitplanes = [array('c') for _ in xrange(bit_depth)]
-
-for row in xrange(height):
-    for byte in xrange(byte_width):
-        planes = [0] * bit_depth
-        for bit in xrange(8):
-            palette_index = img.getpixel((byte * 8 + 7 - bit, row))
-            for plane_index in range(bit_depth):
-                planes[plane_index] |= ((palette_index >> plane_index) & 1) << bit
-        for plane_index in range(bit_depth):
-            bitplanes[plane_index].append(chr(planes[plane_index]))
+        width, bitmap_image.byte_width, height)
 
 if args.separate:
     for plane_index in range(bit_depth):
         with open(args.out % (plane_index + 1), 'wb') as out:
-            write_bitplane(out, plane_index)
+            write_bitplane(out, bitmap_image, plane_index)
 else:
     with open(args.out, 'wb') as out:
-        write_interleaved(out)
+        write_interleaved(out, bitmap_image)
 
 if args.copper:
     with open(args.copper, 'w') as out:
